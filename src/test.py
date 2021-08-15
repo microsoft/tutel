d04747b7e020269284c28527cd98d46e8c530111
Wei CUI <ghostplant@qq.com>
2021-08-15 04:37:26 +0000

    update test.py

--- src/test.py
+++ src/test.py
@@ -8,19 +8,32 @@ from torch import nn
 
 from moe.moe_layer import MOELayer
 
-batch_size = 4
-num_tokens = 512
-model_dim = 2048
-hidden_size = 1024
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--batch-size', type=int, default=4)
+parser.add_argument('--num-tokens', type=int, default=512)
+parser.add_argument('--model-dim', type=int, default=2048)
+parser.add_argument('--hidden-size', type=int, default=1024)
+parser.add_argument('--num-local-experts', type=int, default=2)
+args = parser.parse_args()
+
+
+batch_size = args.batch_size
+num_tokens = args.num_tokens
+model_dim = args.model_dim
+hidden_size = args.hidden_size
+num_local_experts = args.num_local_experts
+
 activation_fn = lambda x: x
-num_local_experts = 2
 
 device = 'cuda'
 torch.manual_seed(1)
 
 torch.set_default_dtype(torch.float32)
 
-'''
+
 class ExpertModel(torch.nn.Module):
     def __init__(self, model_dim, hidden_size, activation_fn = lambda x: x):
         super().__init__()
@@ -33,13 +46,12 @@ class ExpertModel(torch.nn.Module):
         x = self.activation_fn(x)
         x = self.fc2(x)
         return x
-'''
 
 class ExampleModel(torch.nn.Module):
     def __init__(self):
         super().__init__()
         # self._moe_layer = MOELayer('Top1Gate', model_dim, external_experts=[ExpertModel(model_dim, hidden_size, activation_fn) for i in range(num_local_experts)]).to(device)
-        self._moe_layer = MOELayer('Top1Gate', model_dim, builtin_experts={'type': 'ffn', 'count_per_node': num_local_experts, 'hidden_size_per_expert': hidden_size, 'activation_fn': activation_fn}).to(device)
+        self._moe_layer = MOELayer('Top1Gate', model_dim, builtin_experts={'type': 'ffn', 'count_per_node': num_local_experts, 'hidden_size_per_expert': hidden_size, 'activation_fn': activation_fn}, allow_approximation=True).to(device)
 
     def forward(self, input):
         result = self._moe_layer(input)
