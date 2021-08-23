1815889667a571ea1a384090c199c01266afe053
ghostplant <weicu@microsoft.com>
2021-08-23 10:30:03 +0800

    Update run_distributed.sh

--- run_distributed.sh
+++ run_distributed.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -e
 
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
 exec python3 -m torch.distributed.launch --nproc_per_node=${NGPU:-2} ${FILE:-test_tutel.py} "$@"
