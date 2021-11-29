59a1828b4bd416677d49c9ed08e0249f403bf064
Zilong Wang <wangzilong2016@outlook.com>
2021-11-29 20:19:26 +0800

    change default gate to GShardgate

--- tutel/examples/helloworld_fastmoe.py
+++ tutel/examples/helloworld_fastmoe.py
@@ -12,7 +12,9 @@ import torch.distributed as dist
 from torch import nn
 import argparse
 
-from fmoe import FMoETransformerMLP
+from fmoe import FMoE
+from fmoe import FMoELinear
+from fmoe import GShardGate
 
 import logging
 logging.basicConfig(level=logging.INFO)
@@ -69,17 +71,62 @@ elif args.dtype == 'bfloat16':
 else:
     raise Exception('Unrecognized data type specified: %s' % args.dtype)
 
-class ExpertModel(torch.nn.Module):
-    def __init__(self, model_dim, hidden_size, activation_fn):
+    
+class _Expert(nn.Module):
+    r"""
+    An expert using 2 FMoELinear modules to speed up the computation of experts
+    within one worker.
+    """
+
+    def __init__(self, num_expert, d_model, d_hidden, activation, rank=0):
         super().__init__()
-        self.fc1 = torch.nn.Linear(model_dim, hidden_size, bias=True)
-        self.fc2 = torch.nn.Linear(hidden_size, model_dim, bias=True)
-        self.activation_fn = activation_fn
-    def forward(self, x):
-        x = self.fc1(x)
-        x = self.activation_fn(x)
-        x = self.fc2(x)
+        self.htoh4 = FMoELinear(num_expert, d_model, d_hidden, bias=True, rank=rank)
+        self.h4toh = FMoELinear(num_expert, d_hidden, d_model, bias=True, rank=rank)
+        self.activation = activation
+
+    def forward(self, inp, fwd_expert_count):
+        r"""
+        First expand input to 4h (the hidden size is variable, but is called h4
+        for convenience). Then perform activation. Finally shirink back to h.
+        """
+        x = self.htoh4(inp, fwd_expert_count)
+        x = self.activation(x)
+        x = self.h4toh(x, fwd_expert_count)
         return x
+
+
+class FMoETransformerMLP(FMoE):
+    r"""
+    A complete MoE MLP module in a Transformer block.
+    * `activation` is the activation function to be used in MLP in each expert.
+    * `d_hidden` is the dimension of the MLP layer.
+    """
+
+    def __init__(
+        self,
+        num_expert=32,
+        d_model=1024,
+        d_hidden=4096,
+        activation=torch.nn.GELU(),
+        expert_dp_comm="none",
+        expert_rank=0,
+        **kwargs
+    ):
+        super().__init__(num_expert=num_expert, d_model=d_model, **kwargs)
+        self.experts = _Expert(
+            num_expert, d_model, d_hidden, activation, rank=expert_rank, gate=GShardGate
+        )
+        self.mark_parallel_comm(expert_dp_comm)
+
+    def forward(self, inp: torch.Tensor):
+        r"""
+        This module wraps up the FMoE module with reshape, residual and layer
+        normalization.
+        """
+        original_shape = inp.shape
+        inp = inp.reshape(-1, self.d_model)
+        output = super().forward(inp)
+        return output.reshape(original_shape)
     
 class ExampleModel(torch.nn.Module):
     def __init__(self):
