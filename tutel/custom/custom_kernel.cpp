b5500973170e182abe7543807e08648416197b55
Zilong Wang <wangzilong2016@outlook.com>
2021-12-07 13:28:03 +0800

    avoid file permission issue while nvcc compiling

--- tutel/custom/custom_kernel.cpp
+++ tutel/custom/custom_kernel.cpp
@@ -49,7 +49,7 @@ static std::string nvcc_compile(const char* code, const std::string &arch, int c
   if (opendir((home_path + "/.cache/tutel/kernels").c_str()) == nullptr) {
     system(("mkdir -p " + home_path + "/.cache/tutel/kernels").c_str());
   }
-  std::string code_path = home_path + "/.cache/tutel/kernels" + std::to_string(code_id) + "-" + std::to_string(dev_id) + ".cu";
+  std::string code_path = home_path + "/.cache/tutel/kernels/" + std::to_string(code_id) + "-" + std::to_string(dev_id) + ".cu";
   file_write(code_path.data(), code);
 #if !defined(__HIP_PLATFORM_HCC__)
   CHECK_EQ(0, system(("/usr/local/cuda/bin/nvcc " + code_path + " -o " + code_path + ".fatbin --fatbin -O4 -gencode arch=compute_" + arch.substr(3) + ",code=" + arch).c_str()));
