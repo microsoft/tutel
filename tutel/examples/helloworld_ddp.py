e8641858fbd82a24b35ba93f4dd8f0ed7f53f874
Wei CUI <weicu@microsoft.com>
2021-12-09 18:33:01 +0000

    set seeds for megatron & ones gate result comparison

--- tutel/examples/helloworld_ddp.py
+++ tutel/examples/helloworld_ddp.py
@@ -116,6 +116,7 @@ dist_print(model)
 
 optimizer = torch.optim.SGD(model.parameters(), lr=1e-5)
 
+torch.manual_seed(dist_rank)
 x = torch.randn([batch_size, num_tokens, model_dim], device=device, requires_grad=True)
 y = torch.LongTensor(batch_size).random_(1).to(device)
 
