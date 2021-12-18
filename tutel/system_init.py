be605cad948d3b1a1be775fa3972a37616ad7ac3
ghostplant <12099308+ghostplant@users.noreply.github.com>
2021-12-18 04:35:42 +0000

    add initialization interface for data-model parallel (#54)

--- tutel/system_init.py
+++ tutel/system_init.py
@@ -19,3 +19,64 @@ def init_affinity_at_program_beginning():
     except Exception as ex:
         if group_rank == 0:
             logging.warning('Failed to set NUMA status: %s' % ex)
+
+def init_data_model_parallel(group_count=1, backend='nccl'):
+  import torch.distributed as dist
+  try:
+    if dist.is_available():
+      dist.init_process_group(backend=backend)
+    glob_world_size, glob_world_rank = dist.get_world_size(), dist.get_rank()
+    is_distributed = True
+
+    def dist_print(*args):
+        if glob_world_rank == 0:
+            print(*args)
+  except:
+    glob_world_size, glob_world_rank = 1, 0
+    is_distributed = False
+    dist_print = print
+
+  assert glob_world_size % group_count == 0, f"Expected to evenly divide devices into {group_count} groups, while the world size of current sesion is {glob_world_size}."
+
+  dist_group_size = group_count
+  dist_world_size = glob_world_size // dist_group_size
+  dist_world_rank = glob_world_rank % dist_world_size
+  dist_group_rank = glob_world_rank // dist_world_size
+  dist_local_rank = int(os.environ.get('LOCAL_RANK', 0))
+
+  if is_distributed:
+    groups, inner_ranks = [], []
+    for gr in range(dist_group_size):
+      group_ranks = [x for x in range(gr * dist_world_size, (gr + 1) * dist_world_size)]
+      groups += [dist.new_group(ranks=group_ranks)]
+      inner_ranks += [group_ranks]
+    model_group = groups[dist_group_rank]
+
+    groups, outer_ranks = [], []
+    for gr in range(dist_world_size):
+      group_ranks = [x for x in range(gr, dist_world_size * dist_group_size, dist_world_size)]
+      groups += [dist.new_group(ranks=group_ranks)]
+      outer_ranks += [group_ranks]
+
+    data_group = groups[dist_world_rank]
+    global_group = dist.new_group(ranks=list(range(dist_world_size * dist_group_size)))
+  else:
+    model_group, data_group, global_group = None, None, None
+
+  result = init_data_model_parallel
+  result.global_size = glob_world_size
+  result.global_rank = glob_world_rank
+  result.group_count = dist_group_size
+  result.data_rank = dist_group_rank
+  result.model_rank = dist_world_rank
+  result.local_rank = dist_local_rank
+
+  result.data_group = data_group
+  result.model_group = model_group
+  result.global_group = global_group
+
+  result.is_distributed = is_distributed
+  result.dist_print = dist_print
+
+  logging.info(f'Registering device global rank {result.global_rank}: data_rank = {result.data_rank}, model_rank = {result.model_rank}')
+  return result
