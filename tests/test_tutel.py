fe4a0a0b3ef28508ffb82bf729e3086ac5f032d8
Zilong Wang <wangzilong2016@outlook.com>
2021-12-22 12:19:37 +0800

    add a new test case (#59)
    * add a new test case

--- tests/test_tutel.py
+++ tests/test_tutel.py
@@ -9,10 +9,21 @@ import GPUtil
 
 class HelloworldCaller():
     """A class for run tutel helloworld example with different arguments"""
-    def run(self, top, dtype, num_local_experts, show_step_time=True):
+    def run(
+        self,
+        nproc_per_node=1,
+        helloworld_file='helloworld',
+        top=2, dtype='float32',
+        num_local_experts='2',
+        hidden_size=2048,
+        show_step_time=True
+        ):
         """Run helloworld example"""
-        command = 'python3 tutel/examples/helloworld.py --top ' + str(top) + ' --dtype ' + dtype + ' --num_local_experts ' + str(num_local_experts)
-        p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+        if helloworld_file == 'helloworld':
+            command = 'python3 -m torch.distributed.launch --nproc_per_node=' + str(nproc_per_node) + ' tutel/examples/helloworld.py --top ' + str(top) + ' --dtype ' + dtype + ' --num_local_experts ' + str(num_local_experts) + ' --hidden_size ' + str(hidden_size)
+        if helloworld_file == 'helloworld_megatron':
+            command = 'python3 -m torch.distributed.launch --nproc_per_node=' + str(nproc_per_node) + ' tutel/examples/helloworld_megatron.py --dtype ' + dtype + ' --num_local_experts ' + str(num_local_experts) + ' --hidden_size ' + str(hidden_size)
+        p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         losses = []
         while p.poll() is None:
             line = p.stdout.readline().decode("utf8").split()
@@ -22,7 +33,7 @@ class HelloworldCaller():
                         losses.append(round(float(line[4][:-1]), 3))
                     else:
                         losses.append(round(float(line[4][:-1]), 1))
-                if line[0] == '[Summary]':
+                if show_step_time and line[0] == '[Summary]':
                     print('step time:', line[5])
         p.stdout.close()
         return losses
@@ -44,64 +55,63 @@ class TutelTestCase(unittest.TestCase):
 
     def test_top1_fp32_1_expert(self):
         """Test helloworld with top1 gate, float32 dtype and 1 expert(s)."""
-        show_step_time = False
         for i in range(len(self.data[2]['step_time'])):
             if self.data[2]['step_time'][i]['GPU'] in self.GPUtype:
                 print('\nexpected time:', self.data[2]['step_time'][i]['value'])
-        self.assertEqual(self.tutelCaller.run(1, 'float32', 1), self.data[2]['losses'])
+        self.assertEqual(self.tutelCaller.run(nproc_per_node=1, helloworld_file='helloworld', top=1, dtype='float32', num_local_experts=1), self.data[2]['losses'])
 
     def test_top1_fp32_2_experts(self):
         """Test helloworld with top1 gate, float32 dtype and 2 expert(s)."""
-        show_step_time = False
         for i in range(len(self.data[3]['step_time'])):
             if self.data[3]['step_time'][i]['GPU'] in self.GPUtype:
                 print('\nexpected time:', self.data[3]['step_time'][i]['value'])
-        self.assertEqual(self.tutelCaller.run(1, 'float32', 2), self.data[3]['losses'])
+        self.assertEqual(self.tutelCaller.run(nproc_per_node=1, helloworld_file='helloworld', top=1, dtype='float32', num_local_experts=2), self.data[3]['losses'])
 
     def test_top1_fp16_1_expert(self):
         """Test helloworld with top1 gate, float16 dtype and 1 expert(s)."""
-        show_step_time = False
         for i in range(len(self.data[0]['step_time'])):
             if self.data[0]['step_time'][i]['GPU'] in self.GPUtype:
                 print('\nexpected time:', self.data[0]['step_time'][i]['value'])
-        self.assertEqual(self.tutelCaller.run(1, 'float16', 1)[0:2], self.data[0]['losses'][0:2])
+        self.assertEqual(self.tutelCaller.run(nproc_per_node=1, helloworld_file='helloworld', top=1, dtype='float16', num_local_experts=1)[0:2], self.data[0]['losses'][0:2])
 
     def test_top1_fp16_2_experts(self):
         """Test helloworld with top1 gate, float16 dtype and 2 expert(s)."""
-        show_step_time = False
         for i in range(len(self.data[1]['step_time'])):
             if self.data[1]['step_time'][i]['GPU'] in self.GPUtype:
                 print('\nexpected time:', self.data[1]['step_time'][i]['value'])
-        self.assertEqual(self.tutelCaller.run(1, 'float16', 2)[0:2], self.data[1]['losses'][0:2])
+        self.assertEqual(self.tutelCaller.run(nproc_per_node=1, helloworld_file='helloworld', top=1, dtype='float16', num_local_experts=2)[0:2], self.data[1]['losses'][0:2])
 
     def test_top2_fp32_1_expert(self):
         """Test helloworld with top2 gate, float32 dtype and 1 expert(s)."""
-        show_step_time = False
         for i in range(len(self.data[6]['step_time'])):
             if self.data[6]['step_time'][i]['GPU'] in self.GPUtype:
                 print('\nexpected time:', self.data[6]['step_time'][i]['value'])
-        self.assertEqual(self.tutelCaller.run(2, 'float32', 1), self.data[6]['losses'])
+        self.assertEqual(self.tutelCaller.run(nproc_per_node=1, helloworld_file='helloworld', top=2, dtype='float32', num_local_experts=1), self.data[6]['losses'])
 
     def test_top2_fp32_2_experts(self):
         """Test helloworld with top2 gate, float32 dtype and 2 expert(s)."""
-        show_step_time = False
         for i in range(len(self.data[7]['step_time'])):
             if self.data[7]['step_time'][i]['GPU'] in self.GPUtype:
                 print('\nexpected time:', self.data[7]['step_time'][i]['value'])
-        self.assertEqual(self.tutelCaller.run(2, 'float32', 2), self.data[7]['losses'])
+        self.assertEqual(self.tutelCaller.run(nproc_per_node=1, helloworld_file='helloworld', top=2, dtype='float32', num_local_experts=2), self.data[7]['losses'])
 
     def test_top2_fp16_1_expert(self):
         """Test helloworld with top2 gate, float16 dtype and 1 expert(s)."""
-        show_step_time = False
         for i in range(len(self.data[4]['step_time'])):
             if self.data[4]['step_time'][i]['GPU'] in self.GPUtype:
                 print('\nexpected time:', self.data[4]['step_time'][i]['value'])
-        self.assertEqual(self.tutelCaller.run(2, 'float16', 1)[0:2], self.data[4]['losses'][0:2])
+        self.assertEqual(self.tutelCaller.run(nproc_per_node=1, helloworld_file='helloworld', top=2, dtype='float16', num_local_experts=1)[0:2], self.data[4]['losses'][0:2])
 
     def test_top2_fp16_2_experts(self): 
         """Test helloworld with top2 gate, float16 dtype and 2 expert(s)."""
-        show_step_time = False
         for i in range(len(self.data[5]['step_time'])):
             if self.data[5]['step_time'][i]['GPU'] in self.GPUtype:
                 print('\nexpected time:', self.data[5]['step_time'][i]['value'])
-        self.assertEqual(self.tutelCaller.run(2, 'float16', 2)[0:2], self.data[5]['losses'][0:2])
+        self.assertEqual(self.tutelCaller.run(nproc_per_node=1, helloworld_file='helloworld', top=2, dtype='float16', num_local_experts=2)[0:2], self.data[5]['losses'][0:2])
+
+    def test_compare_megatron_with_tutel(self):
+        """Test helloworld_megatron and helloworld which should have same result"""
+        self.assertEqual(
+            self.tutelCaller.run(nproc_per_node=2, helloworld_file='helloworld', top=2, dtype='float32', num_local_experts=-2, show_step_time=False),
+            self.tutelCaller.run(nproc_per_node=2, helloworld_file='helloworld_megatron', dtype='float32', num_local_experts=1, hidden_size=1024, show_step_time=False)
+            )
