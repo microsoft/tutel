70bf0bbc10c20f30fcf045e3e01fd9249afcd31d
Ziyue Yang <ziyyang@microsoft.com>
2022-01-24 17:43:12 +0000

    update ut

--- tests/test_tutel.py
+++ tests/test_tutel.py
@@ -3,6 +3,7 @@
 
 import json
 import unittest
+import os
 import sys, subprocess
 
 import GPUtil
@@ -21,12 +22,15 @@ class HelloworldCaller():
         is_round=True,
         a2a_ffn_overlap_degree=1
         ):
+        # Disable NCCL SHM because it's capacity is limited in Azure pipeline
+        new_env = os.environ.copy()
+        new_env['NCCL_SHM_DISABLE'] = '1'
         """Run helloworld example"""
         if helloworld_file == 'helloworld':
             command = 'python3 -m torch.distributed.launch --nproc_per_node=' + str(nproc_per_node) + ' tutel/examples/helloworld.py --top ' + str(top) + ' --dtype ' + dtype + ' --num_local_experts ' + str(num_local_experts) + ' --hidden_size ' + str(hidden_size) + ' --batch_size ' + str(batch_size) + ' --a2a_ffn_overlap_degree ' + str(a2a_ffn_overlap_degree)
         if helloworld_file == 'helloworld_megatron':
             command = 'python3 -m torch.distributed.launch --nproc_per_node=' + str(nproc_per_node) + ' tutel/examples/helloworld_megatron.py --dtype ' + dtype + ' --num_local_experts ' + str(num_local_experts) + ' --hidden_size ' + str(hidden_size) + ' --batch_size ' + str(batch_size)
-        p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=new_env)
         losses = []
         while p.poll() is None:
             line = p.stdout.readline().decode("utf8").split()
