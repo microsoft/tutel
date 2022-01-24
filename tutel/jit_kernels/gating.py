f03d830b077f73736776bc9164a571e6761ec733
EricWangCN <wangzilong2016@outlook.com>
2022-01-24 15:23:44 +0900

    fix typo

--- tutel/jit_kernels/gating.py
+++ tutel/jit_kernels/gating.py
@@ -10,7 +10,7 @@ from ..impls.jit_compiler import JitCompiler
 disable_fast_cumsum = int(os.environ.get('FAST_CUMSUM', '1')) == 0
 cumsum_kernels = dict()
 
-def torch_cumsum(mask1):
+def torch_cumsum_sub_one(mask1):
   locations1 = torch.cumsum(mask1, dim=0) - 1
   return locations1
 
@@ -18,8 +18,7 @@ def get_cumsum_kernel(samples, global_experts):
 
   if disable_fast_cumsum:
     logging.warning("Optimized cumsum is disabled, and may result in big performance regression.")
-
-    return torch_cumsum
+    return torch_cumsum_sub_one
 
   global cumsum_kernels
   if (samples, global_experts) in cumsum_kernels:
@@ -80,5 +79,5 @@ def fast_cumsum_sub_one(data, dim=0):
   if data.dim() != 2 or dim != 0:
     raise Exception("Unimplemented fast_cumsum_sub_one() of data = %s and dim = %s" % (data.size(), dim))
   if not data.is_cuda:
-    return torch_cumsum(data)
+    return torch_cumsum_sub_one(data)
   return get_cumsum_kernel(data.size(0), data.size(1))(data)
