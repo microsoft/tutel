712bf2e41dd4a62abf1ba06fd94932e252577c2a
ghostplant <ghostplant@qq.com>
2022-02-25 17:52:21 +0000

    update data flow for auto parallel (#105)
    * update data flow for auto parallel
    
    * not scale_samples by default for acceleration

--- tutel/examples/helloworld_amp.py
+++ tutel/examples/helloworld_amp.py
@@ -71,7 +71,7 @@ class ExampleModel(torch.nn.Module):
             scan_expert_func = lambda name, param: setattr(param, 'skip_allreduce', True),
             seeds = (1, dist_rank + 1, 1),
             a2a_ffn_overlap_degree = a2a_ffn_overlap_degree,
-        ).to(device)
+        )
 
         # Summary of different parameter types: gate, local_experts
         local_count = sum([torch.numel(param) for name, param in self._moe_layer.get_parameter_iterator(param_type='local_experts')])
@@ -83,7 +83,7 @@ class ExampleModel(torch.nn.Module):
         result = F.log_softmax(torch.sum(result, dim=2), dim=1)
         return result
 
-model = ExampleModel()
+model = ExampleModel().to(device)
 dist_print(model)
 
 optimizer = torch.optim.SGD(model.parameters(), lr=1e-5)
