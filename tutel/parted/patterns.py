e09c2aaf7967335b60829b1a10dbc6c04bf6f6f3
ghostplant <ghostplant@qq.com>
2022-03-12 11:14:02 +0000

    update parted settings (#111)

--- tutel/parted/patterns.py
+++ tutel/parted/patterns.py
@@ -10,7 +10,7 @@ def is_partition(dim):
   return dim >= 0
 
 @register_primitive("BAR")
-def primitive_pass_through(node, output_dim, group_size, rank):
+def primitive_pass_through(sess, node, output_dim, group_size, rank):
   if not is_replicas(output_dim) and not is_partition(output_dim):
     return
   source_dims, num_partitions = node.parser.emit_dims_by_id(output_dim)
@@ -19,11 +19,11 @@ def primitive_pass_through(node, output_dim, group_size, rank):
     yield (0, source_dims, {})
     return
 
-  connectors = dict([(inp, f'warp_bwd_allreduce($, {node.inputs[inp].op_type == "param"})') for inp in source_dims if is_replicas(source_dims[inp])])
+  connectors = dict([(inp, sess.backend.link('$', -1, None, is_param=(node.inputs[inp].op_type == "param"))) for inp in source_dims if is_replicas(source_dims[inp])])
   yield (0, source_dims, connectors)
 
 @register_primitive("FAR")
-def primitive_fwd_allreduce_sum(node, output_dim, group_size, rank):
+def primitive_fwd_allreduce_sum(sess, node, output_dim, group_size, rank):
   if not is_replicas(output_dim):
     return
   if node.parser.reduce_type != '+':
@@ -37,12 +37,12 @@ def primitive_fwd_allreduce_sum(node, output_dim, group_size, rank):
     except NotImplementedError:
       continue
     assert num_partitions > 0, "It is unexpected that no certain input is parted."
-    connectors = dict([(inp, f'warp_bwd_allreduce($, {node.inputs[inp].op_type == "param"})') for inp in source_dims if is_replicas(source_dims[inp])])
-    connectors[''] = f'C.PrimFwdAllreduce.apply(default_group, $)'
+    connectors = dict([(inp, sess.backend.link('$', -1, None, is_param=(node.inputs[inp].op_type == "param"))) for inp in source_dims if is_replicas(source_dims[inp])])
+    connectors[''] = sess.backend.link('$', None, -1)
     yield (i, source_dims, connectors)
 
 @register_primitive("RS")
-def primitive_fwd_reduce_scatter_sum(node, output_dim, group_size, rank):
+def primitive_fwd_reduce_scatter_sum(sess, node, output_dim, group_size, rank):
   if not is_partition(output_dim):
     return
   if node.parser.reduce_type != '+':
@@ -56,21 +56,21 @@ def primitive_fwd_reduce_scatter_sum(node, output_dim, group_size, rank):
     except NotImplementedError:
       continue
     assert num_partitions > 0, "It is unexpected that no certain input is parted."
-    connectors = dict([(inp, f'warp_bwd_allreduce($, {node.inputs[inp].op_type == "param"})') for inp in source_dims if is_replicas(source_dims[inp])])
-    connectors[''] = f'C.PrimReducescatter.transform(default_group, $, {output_dim})'
+    connectors = dict([(inp, sess.backend.link('$', -1, None, is_param=(node.inputs[inp].op_type == "param"))) for inp in source_dims if is_replicas(source_dims[inp])])
+    connectors[''] = sess.backend.link('$', None, output_dim)
     yield (i, source_dims, connectors)
 
 @register_primitive("SPLIT")
-def primitive_fwd_spatial_split(node, output_dim, group_size, rank):
+def primitive_fwd_spatial_split(sess, node, output_dim, group_size, rank):
   if not is_partition(output_dim):
     return
   source_dims, num_partitions = node.parser.emit_dims_by_id(-1)
   assert num_partitions == 0, "It is unexpected that certain input is parted."
-  connectors = dict([('', f'C.PrimSpatialSplit.transform(default_group, $, {output_dim})')])
+  connectors = dict([('', sess.backend.link('$', -1, output_dim))])
   yield (0, source_dims, connectors)
 
 @register_primitive("AG")
-def primitive_fwd_all_gather(node, output_dim, group_size, rank):
+def primitive_fwd_all_gather(sess, node, output_dim, group_size, rank):
   if not is_replicas(output_dim):
     return
   for i in range(len(node.shape)):
@@ -84,12 +84,12 @@ def primitive_fwd_all_gather(node, output_dim, group_size, rank):
       continue
     if num_partitions == 0: # Handled by fwd_pass_through as well
       continue
-    connectors = dict([(inp, f'warp_bwd_allreduce($, {node.inputs[inp].op_type == "param"})') for inp in source_dims if is_replicas(source_dims[inp])])
-    connectors[''] = f'C.PrimAllgather.transform(default_group, $, {rank})'
+    connectors = dict([(inp, sess.backend.link('$', -1, None, is_param=(node.inputs[inp].op_type == "param"))) for inp in source_dims if is_replicas(source_dims[inp])])
+    connectors[''] = sess.backend.link('$', rank, -1)
     yield (i, source_dims, connectors)
 
 @register_primitive("A2A")
-def primitive_alltoall(node, output_dim, group_size, rank):
+def primitive_alltoall(sess, node, output_dim, group_size, rank):
   if not is_partition(output_dim):
     return
   shape = node.shape
@@ -102,14 +102,14 @@ def primitive_alltoall(node, output_dim, group_size, rank):
       continue
     try:
       source_dims, num_partitions = node.parser.emit_dims_by_id(i)
-      connectors = dict([(inp, f'warp_bwd_allreduce($, {node.inputs[inp].op_type == "param"})') for inp in source_dims if is_replicas(source_dims[inp])])
-      connectors[''] = f'C.PrimAllToAll.transform(default_group, $, input_dim={i}, output_dim={output_dim})'
+      connectors = dict([(inp, sess.backend.link('$', -1, None, is_param=(node.inputs[inp].op_type == "param"))) for inp in source_dims if is_replicas(source_dims[inp])])
+      connectors[''] = sess.backend.link('$', i, output_dim)
       yield (i, source_dims, connectors)
     except NotImplementedError:
       continue
 
 @register_primitive("ZERO")
-def primitive_zero(node, output_dim, group_size, rank):
+def primitive_zero(sess, node, output_dim, group_size, rank):
   if not is_partition(output_dim):
     return
   source_dims, num_partitions = node.parser.emit_dims_by_id(output_dim)
@@ -121,9 +121,9 @@ def primitive_zero(node, output_dim, group_size, rank):
       continue
     if node.inputs[inp].op_type == 'param':
       source_dims[inp] = -2
-      has_params, connectors[inp] = True, f'C.PrimAllgather.zero_param(default_group, $, {node.inputs[inp].shape})'
+      has_params, connectors[inp] = True, sess.backend.link('$', -2, -1, output_shape=node.inputs[inp].shape)
     else:
-      connectors[inp] = f'warp_bwd_allreduce($, {node.inputs[inp].op_type == "param"})'
+      connectors[inp] = sess.backend.link('$', -1, None, is_param=(node.inputs[inp].op_type == "param"))
   if not has_params:
     return
   yield (0, source_dims, connectors)
