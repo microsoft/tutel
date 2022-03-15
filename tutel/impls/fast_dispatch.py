a6ae4eaf11bf8ca8b8efa2e2be4e7657d211bc2f
Wei CUI <ghostplant@qq.com>
2022-03-15 18:08:05 +0800

    expose interface for extract_critical, fast_encode, fast_decode

--- tutel/impls/fast_dispatch.py
+++ tutel/impls/fast_dispatch.py
@@ -8,6 +8,7 @@ from torch import Tensor
 
 from .jit_compiler import IS_HIP_EXTENSION
 from ..jit_kernels import sparse as jit_kernel
+from ..jit_kernels.gating import fast_cumsum_sub_one
 
 class GatingEncoder(torch.autograd.Function):
     @staticmethod
@@ -121,3 +122,75 @@ class TutelMoeFastDispatcher:
             return GatingDecoder.apply(self, data.to(self.dtype)).to(self.original_dtype)
 
 fast_dispatcher = TutelMoeFastDispatcher
+
+def one_hot_with_dtype(data, num_classes, dtype):
+    result = torch.zeros([data.size(0), num_classes], device=data.device, dtype=dtype)
+    result.scatter_(1, data.unsqueeze(-1), 1)
+    return result
+
+def compute_sorted_location(x, importance_scores):
+    sorted_x = x[importance_scores.argsort(dim=0)]
+    sorted_cumsum = fast_cumsum_sub_one(sorted_x) * sorted_x
+    return sorted_cumsum[importance_scores.argsort(dim=0).argsort(dim=0)]
+
+def load_balance(gates, mask1, num_global_experts, fp32_gate):
+    if gates.dtype == torch.float32 or fp32_gate:
+        me = torch.sum(gates.float(), dim=0)
+        ce = torch.sum(mask1.to(me.dtype), dim=0)
+        l_loss = torch.sum(me * ce) * (num_global_experts / (gates.size(0) * gates.size(0)))
+    else:
+        me = torch.mean(gates, dim=0)
+        ce = torch.mean(mask1.to(gates.dtype), dim=0)
+        l_loss = torch.sum(me * ce) * num_global_experts
+    return l_loss
+
+def extract_critical(gates, top_k, capacity_factor=1.0, fp32_gate=False, batch_prioritized_routing=False):
+    topk_indices = torch.topk(gates, top_k, dim=1).indices
+    num_global_experts = gates.size(1)
+
+    indices_s = [x.view(-1) for x in topk_indices.chunk(top_k, dim=1)]
+    masks_se = [one_hot_with_dtype(x, num_classes=num_global_experts, dtype=x.dtype) for x in indices_s]
+    gates_s = [(gates * x).sum(dim=1) for x in masks_se]
+
+    l_loss = load_balance(gates, masks_se[0], num_global_experts, fp32_gate)
+
+    if batch_prioritized_routing:
+        importance_scores = -1 * gates.max(dim=1)[0]
+        compute_location = lambda x: compute_sorted_location(x, importance_scores)
+    else:
+        compute_location = fast_cumsum_sub_one
+
+    locations1 = compute_location(masks_se[0])
+
+    locations_s = [torch.sum(locations1 * masks_se[0], dim=1).to(torch.int32)]
+
+    if top_k > 1:
+        acc_base = None
+        for k in range(1, top_k):
+            acc_base = torch.sum(masks_se[k - 1], dim=0, keepdim=True) if acc_base is None else acc_base + torch.sum(masks_se[k - 1], dim=0, keepdim=True)
+            locations2 = compute_location(masks_se[k])
+            locations2 += acc_base
+            locations_s.append(torch.sum(locations2 * masks_se[k], dim=1).to(torch.int32))
+
+        # Normalize Gate
+        denom_s = torch.clamp(sum(gates_s), min=torch.finfo(gates_s[0].dtype).eps)
+        gates_s = [x / denom_s for x in gates_s]
+
+    indices_s = [x.to(torch.int32) for x in indices_s]
+
+    capacity = top_k * int(capacity_factor * ((gates.size(0) + num_global_experts - 1) // num_global_experts))
+    return (num_global_experts, indices_s, locations_s, gates_s, capacity), l_loss
+
+
+def fast_encode(data, critial_data, is_postscore=True):
+    num_global_experts = critial_data[0]
+    dispatcher = TutelMoeFastDispatcher(num_global_experts, 0, data.size(-1), data.dtype)
+    dispatcher.update(*critial_data[1:], is_postscore=is_postscore)
+    return dispatcher.encode(data)
+
+def fast_decode(data, critial_data, is_postscore=True):
+    num_global_experts = critial_data[0]
+    dispatcher = TutelMoeFastDispatcher(num_global_experts, 0, data.size(-1), data.dtype)
+    dispatcher.update(*critial_data[1:], is_postscore=is_postscore)
+    return dispatcher.decode(data)
+
