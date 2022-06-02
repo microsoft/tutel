747ab117bf89129ed5bc02afbf123b516a268532
Your Name <liuze@mail.ustc.edu.cn>
2022-06-02 23:44:26 +0800

    add cosine router; add load loss and importance loss in arXiv:1701.06538

--- tutel/impls/losses.py
+++ tutel/impls/losses.py
@@ -2,6 +2,7 @@
 # Licensed under the MIT license.
 
 import torch
+from torch.distributions.normal import Normal
 
 def _one_hot_with_dtype(data, num_classes, dtype, hot_value=1):
     result = torch.zeros([data.size(0), num_classes], device=data.device, dtype=dtype)
@@ -16,3 +17,26 @@ def gshard_loss(scores_w_noise, top_ids):
     ce = torch.sum(mask, dim=0)
     l_aux = torch.sum(me * ce) / num_samples
     return l_aux
+
+def load_importance_loss(scores_wo_noise, topk_logits, num_global_experts, gate_noise):
+    def load_loss(scores_wo_noise, topk_logits, num_global_experts, gate_noise):
+        normal = Normal(
+            torch.tensor([0.0], device=scores_wo_noise.device),
+            torch.tensor([gate_noise / num_global_experts], device=scores_wo_noise.device),
+        )
+        threshold = topk_logits[:, -1].view(-1, 1).float()
+        diff = scores_wo_noise.float() - threshold.float()
+        prob = normal.cdf(diff)
+        Load = prob.sum(0)
+        l_load = Load.float().var() / (Load.float().mean() ** 2 + 1e-10)
+        return l_load
+
+    def importance_loss(scores_wo_noise):
+        Impi = scores_wo_noise.float().sum(0)
+        l_imp = Impi.float().var() / (Impi.float().mean() ** 2 + 1e-10)
+
+        return l_imp
+
+    l_imp = importance_loss(scores_wo_noise)
+    l_load = load_loss(scores_wo_noise, topk_logits, num_global_experts, gate_noise)
+    return (l_imp + l_load) / 2.0
