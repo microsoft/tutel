f81c2a6df4800cfc43023c7efa00f978a8644ced
Your Name <liuze@mail.ustc.edu.cn>
2022-06-03 00:07:52 +0800

    fix a bug

--- tutel/gates/top.py
+++ tutel/gates/top.py
@@ -6,7 +6,7 @@ import torch
 class LinearTopKGate(torch.nn.Module):
     def __init__(self, model_dim, num_global_experts, k=1, fp32_gate=False, **options):
         super().__init__()
-        self.wg = torch.nn.Linear(model_dim, num_global_experts, bias=False, dtype=torch.float32 if fp32_gate else None)
+        self.wg = torch.nn.Linear(model_dim, num_global_experts, bias=False)
         self.top_k = min(num_global_experts, int(k))
         self.fp32_gate = fp32_gate
 
